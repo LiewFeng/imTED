02d102ee435cbf66a135304745065b20b8ddec0e
LiuFeng <42316773+LiewFeng@users.noreply.github.com>
2022-12-12 19:29:34 +0800

    add code
    add the code of layer decay for few shot task

--- mmcv_custom/__init__.py
+++ mmcv_custom/__init__.py
@@ -2,6 +2,6 @@
 
 from .checkpoint import load_checkpoint
 from .layer_decay_optimizer_constructor import LayerDecayOptimizerConstructor
+from .layer_decay_optimizer_constructor_backbone_frozen import LayerDecayOptimizerConstructorBackboneFronzen
 
-
-__all__ = ['load_checkpoint', 'LayerDecayOptimizerConstructor']
+__all__ = ['load_checkpoint', 'LayerDecayOptimizerConstructor', 'LayerDecayOptimizerConstructorBackboneFronzen']
